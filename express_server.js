d79ba168d92d5b38f67b4b6330791936a781c8dc
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-28 02:43:27 +0000

    Added functionality for adding a new shortURL

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+// const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -9,6 +10,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//generate RandomString of 'n' number of characters
+const generateRandomString = function(n) {
+  let retStr = "";
+  const min = 48;
+  const max = 122;
+  for (let i = 0; i < n; i++) {
+    randNum = Math.floor(Math.random() * (max - min + 1) + min);
+    if (!((57 < randNum && randNum < 65) || (90 < randNum && randNum < 97))) {
+      retStr += String.fromCharCode(randNum);
+    } else { 
+      i--;
+    }
+  }
+  return retStr;
+}
+
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -23,7 +41,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get(`/urls/:shortURL`, (req, res) => {
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = req.body.longURL;
+  // console.log(req.body);
+  res.send(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const templateVars = { shortURL, longURL: urlDatabase[shortURL]};
   res.render('urls_show', templateVars);
